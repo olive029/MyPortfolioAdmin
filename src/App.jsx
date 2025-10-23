211446546833cb2a909af75ed01bccd2bcd1b08d
olive029 <omunjogu@gmail.com>
2025-10-23 16:19:14 +0300

    Refactor App component with nested routes and Helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
 // App.jsx
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AnimatedBackground from './components/AnimatedBackground'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+
+// Layout component for auth routes
+const AuthLayout = () => {
+  return (
+    <div>
+      <Outlet /> {/* This renders the child routes */}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AnimatedBackground />
+    <HelmetProvider>
+      <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          {/* Nested auth routes */}
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="dashboard/*" element={<Dashboard />} />
+          </Route>
+          
+          {/* Redirects */}
           <Route path="/" element={<Login />} />
+          <Route path="/admin" element={<Login />} />
+          <Route path="*" element={<Login />} />
         </Routes>
-      </div>
-    </Router>
-  )
+      </Router>
+    </HelmetProvider>
+  );
 }
 
-export default App
+export default App;
